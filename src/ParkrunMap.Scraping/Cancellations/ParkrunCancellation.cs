c43d1c53bc270de6e4a2e7186b343f3048072bfe
kevbite <kev_bite@msn.com>
2018-12-29 23:44:53 +0000

    Adding in cancellations parser

---
+++ src/ParkrunMap.Scraping/Cancellations/ParkrunCancellation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ParkrunMap.Scraping.Cancellations
+{
+    public class ParkrunCancellation
+    {
+        public DateTime Date { get; }
+
+        public string Name { get; }
+
+        public Uri Uri { get; }
+
+        public string Reason { get; }
+
+        public ParkrunCancellation(DateTime date, string name, Uri uri, string reason)
+        {
+            Date = date;
+            Name = name;
+            Uri = uri;
+            Reason = reason;
+        }
+    }
+}
