aefc7040f044b0d612e16a8f80c3b11100277d72
kevbite <kev_bite@msn.com>
2018-12-29 14:20:20 +0000

    Green icons for all

--- src/ParkrunMap.UI/ParkrunMap.UI/ClientApp/src/components/Map.js
+++ src/ParkrunMap.UI/ParkrunMap.UI/ClientApp/src/components/Map.js
@@ -3,6 +3,15 @@ import { connect } from 'react-redux';
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+function getPinIcon(parkrun){
+  const greenPinIcon = "https://maps.google.com/mapfiles/ms/icons/green-dot.png";
+  const orangePinIcon = "https://maps.google.com/mapfiles/ms/icons/orange-dot.png";
+  const redPinIcon = "https://maps.google.com/mapfiles/ms/icons/red-dot.png";
+  
+  // Everything is green for moment.
+  return greenPinIcon;
+};
+
 const ParkrunMap = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
@@ -23,7 +32,8 @@ const ParkrunMap = compose(
     defaultCenter={{ lat: props.center.latitude, lng: props.center.longitude}}
     center={{ lat: props.center.latitude, lng: props.center.longitude}}
   >
-    {props.parkruns && props.parkruns.map(parkrun => <Marker key={parkrun.name} position={{ lat: parkrun.lat, lng: parkrun.lon }} onClick={props.onParkrunMarkerClick} />)}
+    {props.parkruns &&
+      props.parkruns.map(parkrun => <Marker key={parkrun.name} position={{ lat: parkrun.lat, lng: parkrun.lon }} icon={getPinIcon(parkrun)} onClick={props.onParkrunMarkerClick} />)}
   </GoogleMap>
 );
 
