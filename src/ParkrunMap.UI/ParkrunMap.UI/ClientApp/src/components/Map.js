4e7c0e3a93250cfbf99919d704807ebb04b677a0
kevbite <kev_bite@msn.com>
2018-12-31 16:16:02 +0000

    Using coloured pins markers to show cancellations

--- src/ParkrunMap.UI/ParkrunMap.UI/ClientApp/src/components/Map.js
+++ src/ParkrunMap.UI/ParkrunMap.UI/ClientApp/src/components/Map.js
@@ -35,8 +35,28 @@ class ParkrunMarker extends React.Component {
     const orangePinIcon = "https://maps.google.com/mapfiles/ms/icons/orange-dot.png";
     const redPinIcon = "https://maps.google.com/mapfiles/ms/icons/red-dot.png";
 
-    // Everything is green for moment.
-    return greenPinIcon;
+    function calculateNextSaturday(){
+      var now = new Date();    
+      now.setDate(now.getDate() + (6+(7-now.getDay())) % 7);
+      now.setHours(0,0,0,0);
+
+      return now;
+  }
+
+  const nextSaturday = calculateNextSaturday();
+  
+  const cancellations = this.props.parkrun.cancellations.map(x =>{
+    var d = new Date(x.date);
+    d.setHours(0,0,0,0);
+    return d;
+  });
+  if(cancellations.findIndex(x => x.valueOf() === nextSaturday.valueOf()) >= 0){
+    return redPinIcon;
+  }else if(cancellations.length){
+    return orangePinIcon;
+  }
+
+  return greenPinIcon;
   };
 
   render() {
@@ -49,7 +69,20 @@ class ParkrunMarker extends React.Component {
         {this.state.showInformation &&
           <InfoWindow
             onCloseClick={this.toggle}>
-                <strong>{this.props.parkrun.name}</strong>
+            <div>
+              <strong>{this.props.parkrun.name}</strong>
+              <ul>
+                <li><a href={this.props.parkrun.uri}>Website</a></li>
+                {this.props.parkrun.cancellations.length > 0 &&
+                   <li>
+                     <div class="warning">Cancellations</div>
+                     <ul>
+                     {this.props.parkrun.cancellations.map(x => <li>{new Date(x.date).toDateString()}</li>)}
+                     </ul>
+                   </li>
+                }
+              </ul>
+            </div>
           </InfoWindow>}
 
       </Marker>);
