a27cbc9c1741c6e3ae40bc2fb02f099ea81bb9b9
kevbite <kev_bite@msn.com>
2019-01-02 20:02:52 +0000

    Adding Google Analytics, closes #10

--- src/ParkrunMap.UI/ParkrunMap.UI/ClientApp/src/App.js
+++ src/ParkrunMap.UI/ParkrunMap.UI/ClientApp/src/App.js
@@ -3,10 +3,11 @@ import { Route } from 'react-router';
 import Layout from './components/Layout';
 import ParkrunMap from './components/ParkrunMap';
 import About from './components/About';
+import withTracker from './withTracker';
 
 export default () => (
   <Layout>
-    <Route exact path='/' component={ParkrunMap} />
-    <Route exact path='/about' component={About} />
+    <Route exact path='/' component={withTracker(ParkrunMap)} />
+    <Route exact path='/about' component={withTracker(About)} />
   </Layout>
 );
