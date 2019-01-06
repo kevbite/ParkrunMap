364ca334fc3af832bfd18123c9f423fac4e5106c
kevbite <kev_bite@msn.com>
2019-01-06 23:08:27 +0000

    Dealing with edge case of iframe src's whereby we need to follow the uri until we get to a page that is a 200 and then grab the query string

--- src/ParkrunMap.Scraping/Course/CourseParser.cs
+++ src/ParkrunMap.Scraping/Course/CourseParser.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.WebUtilities;
 
@@ -8,7 +12,9 @@ namespace ParkrunMap.Scraping.Course
 {
     public class CourseParser
     {
-        public object Parse(Stream stream)
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public async Task<CourseDetails> Parse(Stream stream)
         {
             var htmlDoc = new HtmlDocument();
             htmlDoc.Load(stream);
@@ -17,21 +23,56 @@ namespace ParkrunMap.Scraping.Course
 
             var description = ParseDescription(headers);
 
-            var googleMapId = ParseGoogleMapId(headers);
+            var googleMapId = await ParseGoogleMapId(headers);
 
             return new CourseDetails(description, googleMapId);
         }
 
-        private static string ParseGoogleMapId(HtmlNodeCollection headers)
+        private static async Task<string> ParseGoogleMapId(HtmlNodeCollection headers)
         {
             var courseMapH1 = headers.First(x => x.InnerText == "Course Map");
 
-            var uri = courseMapH1.SelectSingleNode("following-sibling::iframe").GetAttributeValue("src", null);
+            var uri = courseMapH1.SelectSingleNode("following-sibling::iframe").Attributes["src"].DeEntitizeValue;
 
-            var query = QueryHelpers.ParseQuery(new Uri(uri).Query);
-            var googleMapId = query["mid"];
+            if (TryParseGoogleMapId(uri, out var stringValues))
+            {
+                return stringValues;
+            }
 
-            return googleMapId;
+            return await FollowUri(uri);
+        }
+
+        private static async Task<string> FollowUri(string uri)
+        {
+            var httpResponseMessage = await HttpClient.GetAsync(uri)
+                .ConfigureAwait(false);
+
+            if (TryParseGoogleMapId(httpResponseMessage.RequestMessage.RequestUri, out var value))
+            {
+                return value;
+            }
+
+            throw new Exception($"Could not find Google Map Id after following course map iframe src {uri}");
+        }
+
+        private static bool TryParseGoogleMapId(Uri uri, out string value)
+        {
+            var query = QueryHelpers.ParseQuery(uri.Query);
+
+            if (query.TryGetValue("mid", out var googleMapId))
+            {
+                value = googleMapId;
+                return true;
+            }
+
+            value = null;
+            return false;
+        }
+
+
+        private static bool TryParseGoogleMapId(string uri, out string value)
+        {
+            return TryParseGoogleMapId(new Uri(uri), out value);
         }
 
         private static string ParseDescription(HtmlNodeCollection headers)
--- test/ParkrunMap.Scraping.Tests/Course/CourseParserTests.cs
+++ test/ParkrunMap.Scraping.Tests/Course/CourseParserTests.cs
@@ -1,26 +1,47 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using FluentAssertions;
+using ParkrunMap.Scraping.Course;
 using Xunit;
 
 namespace ParkrunMap.Scraping.Tests.Course
 {
     public class CourseParserTests
     {
-        [Fact]
-        public void ShouldParseCourseDetails()
+        [Theory]
+        [MemberData(nameof(Data))]
+        public async Task ShouldParseCourseDetails(string filePath, string description, string googleMapId)
         {
-            using (var cancellationsPage = File.OpenRead(@".\data\heslington.course.html"))
+            using (var cancellationsPage = File.OpenRead(filePath))
             {
                 var parser = new CourseParser();
-                var course = parser.Parse(cancellationsPage);
+                var course = await parser.Parse(cancellationsPage);
 
                 course.Should().BeEquivalentTo(new
                 {
-                    Description = "One lap of the 1km tarmac cycle circuit followed by 3km out and back on block paved Lakeside Way and a final lap of the 1km tarmac cycle circuit. The start and finish is approximately 10-minute walk from the car park.",
-                    GoogleMapId = "1BscG9Q0CTyJzJ217yn-dtztpG8CAOa6h"
+                    Description = description,
+                    GoogleMapId = googleMapId
                 });
             }
 
         }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    @".\data\heslington.course.html",
+                    "One lap of the 1km tarmac cycle circuit followed by 3km out and back on block paved Lakeside Way and a final lap of the 1km tarmac cycle circuit. The start and finish is approximately 10-minute walk from the car park.",
+                    "1BscG9Q0CTyJzJ217yn-dtztpG8CAOa6h"
+                },
+                new object[]
+                {
+                    @".\data\york.course.html",
+                    "1.5 laps (approximately) of the tarmac service road around the inside of the racecourse. Very flat, with few turns, making it a very fast course. On course map, start at green pin and head anti-clockwise round service road. Complete 1 full lap, then continue on round approximately another 1/2 lap to red Finish pin",
+                    "1i-izH2wIlADvEGjfrjQ-zSfg3yc"
+                },
+            };
     }
 }
