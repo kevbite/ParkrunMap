63280da58d1e6879dbb8850866055a5a1f5158da
Sakis Koltsidas <sakis.koltsidas@gmail.com>
2020-02-16 20:42:49 +0000

    Refactor parser to return a read only collection of parkruns

--- src/ParkrunMap.Scraping/Parkruns/ParkRunEventsJsonParser.cs
+++ src/ParkrunMap.Scraping/Parkruns/ParkRunEventsJsonParser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 using ParkrunMap.Scraping.Parkruns.Data;
 
@@ -7,26 +8,23 @@ namespace ParkrunMap.Scraping.Parkruns
 {
     public static class ParkRunEventsJsonParser
     {
-        public static IEnumerable<Parkrun> Parse(Stream stream)
+        public static IReadOnlyCollection<Parkrun> Parse(Stream stream)
         {
             using var reader = new StreamReader(stream);
-            
-            if (!(JsonSerializer.Create().Deserialize(reader, typeof(ParkRunEventsByCountry)) is ParkRunEventsByCountry parkRuns))
-                yield break;
 
-            foreach (var parkRun in parkRuns.Events.Features)
-            {
-                yield return new Parkrun(
-                    geoXmlId: parkRun.Id,
-                    name: parkRun.Properties.EventShortName,
-                    websiteDomain: parkRuns.Countries[parkRun.Properties.CountryCode.ToString()].Url,
-                    websitePath: $"/{parkRun.Properties.EventName}",
-                    region: null,
-                    country: null,
-                    latitude: parkRun.Geometry.Coordinates[1],
-                    longitude: parkRun.Geometry.Coordinates[0]
-                );
-            }
+            return JsonSerializer.Create().Deserialize(reader, typeof(ParkRunEventsByCountry)) is ParkRunEventsByCountry parkRuns
+                ? parkRuns.Events.Features
+                    .Select(p => new Parkrun(
+                        geoXmlId: p.Id,
+                        name: p.Properties.EventShortName,
+                        websiteDomain: parkRuns.Countries[p.Properties.CountryCode.ToString()].Url,
+                        websitePath: $"/{p.Properties.EventName}",
+                        region: null,
+                        country: null,
+                        latitude: p.Geometry.Coordinates[1],
+                        longitude: p.Geometry.Coordinates[0]))
+                    .ToList()
+                : new List<Parkrun>();
         }
     }
 }
