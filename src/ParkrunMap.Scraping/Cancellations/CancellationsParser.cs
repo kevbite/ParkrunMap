10724a8685b151858a12511247e9e8f13699f18f
Kevin Smith <kev_bite@msn.com>
2022-12-24 20:59:57 +0000

    Fixing up CancellationsParser

--- src/ParkrunMap.Scraping/Cancellations/CancellationsParser.cs
+++ src/ParkrunMap.Scraping/Cancellations/CancellationsParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
 namespace ParkrunMap.Scraping.Cancellations
@@ -15,23 +16,26 @@ namespace ParkrunMap.Scraping.Cancellations
             var htmlDoc = new HtmlDocument();
             htmlDoc.Load(stream);
 
-            var h1s = htmlDoc.DocumentNode.SelectNodes("//div[@id='content']//h1");
+            var h2s = htmlDoc.DocumentNode.SelectNodes("//div[@id='content']//h2");
 
-            foreach (var h1 in h1s)
+            foreach (var h2 in h2s.Skip(1))
             {
-                if (DateTime.TryParseExact(h1.InnerText, "yyyy-MM-dd", CultureInfo.InvariantCulture,
+                if (DateTime.TryParseExact(h2.InnerText, "dddd, d MMMM yyyy", CultureInfo.InvariantCulture,
                     DateTimeStyles.AssumeUniversal, out var date))
                 {
-                    var listItems = h1.NextSibling.ChildNodes;
+                    var listItems = h2.SelectSingleNode("following-sibling::ul")
+                        .SelectNodes("./li");
 
                     foreach (var listItem in listItems)
                     {
                         var link = listItem.SelectSingleNode("a");
                         var parkrunTitle = link.InnerText;
                         var parkrunUri = link.Attributes["href"].Value;
-                        var reason = listItem.InnerText.Split(':').Last().Trim();
-
-                        if (parkrunUri.EndsWith("-juniors/"))
+                        
+                        var regex = new Regex("[ \r\n]{2,}", RegexOptions.None);
+                        var reason = regex.Replace(listItem.InnerText.Split(':').Last(), " ").Trim();;
+                        
+                        if (parkrunUri.EndsWith("-juniors"))
                         {
                             // We're ignoring junior parkruns
                             continue;
--- test/ParkrunMap.Scraping.Tests/Cancellations/CancellationsParserTests.cs
+++ test/ParkrunMap.Scraping.Tests/Cancellations/CancellationsParserTests.cs
@@ -18,40 +18,17 @@ namespace ParkrunMap.Scraping.Tests.Cancellations
 
                 cancellations.Should().BeEquivalentTo(new[]
                 {
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Bodelwyddan Castle parkrun",
-                        "www.parkrun.org.uk","/bodelwyddancastle", "Estate closed for festive period"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Dudley parkrun",
-                        "www.parkrun.org.uk","/dudley", "Dell Stadium closed due to DMBC shutdown over Xmas"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Hay Lodge parkrun",
-                        "www.parkrun.org.uk","/haylodge", "High winds"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Lochore Meadows parkrun",
-                        "www.parkrun.org.uk","/lochoremeadows", "Unknown (HQ)"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Mulbarton parkrun",
-                        "www.parkrun.org.uk","/mulbarton", "Protocols to install new ED"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Newent parkrun, Forest of Dean",
-                        "www.parkrun.org.uk","/newent", "School grounds are closed for Christmas holiday"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Omagh parkrun",
-                        "www.parkrun.org.uk","/omagh", "course closed"),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Sandwell Valley parkrun",
-                        "www.parkrun.org.uk","/sandwellvalley", "No access to an AED as the Park Farm is closed."),
-                    new ParkrunCancellation(new DateTime(2018,12,29), "Vogrie parkrun",
-                        "www.parkrun.org.uk","/vogrie", "Run cancelled due to high winds forecast"),
+                    new ParkrunCancellation(new DateTime(2022, 12, 24), "Babbs Mill parkrun",
+                        "www.parkrun.org.uk", "/babbsmill", "Following the tragic incident on Sunday 11th"),
+                    new ParkrunCancellation(new DateTime(2022, 12, 24), "Bedworth parkrun",
+                        "www.parkrun.org.uk", "/bedworth", "Ice, on the course"),
+                    new ParkrunCancellation(new DateTime(2022, 12, 24), "Blickling parkrun",
+                        "www.parkrun.org.uk", "/blickling", "Park unavailable to events"),
 
-                    new ParkrunCancellation(new DateTime(2019,01,05), "Bodelwyddan Castle parkrun",
-                        "www.parkrun.org.uk","/bodelwyddancastle", "Run Director not available - Cross Country Champs"),
-                    new ParkrunCancellation(new DateTime(2019,01,05), "Bramley parkrun",
-                        "www.parkrun.org.uk","/bramley", "removal of asbestos from a park facility"),
-                    new ParkrunCancellation(new DateTime(2019,01,05), "Witton parkrun",
-                        "www.parkrun.org.uk","/witton", "Cross Country Event in the park"),
-
-                    new ParkrunCancellation(new DateTime(2019,01,12), "Bramley parkrun",
-                        "www.parkrun.org.uk","/bramley", "removal of asbestos from a park facility"),
-                    new ParkrunCancellation(new DateTime(2019,01,12), "Prospect parkrun",
-                        "www.parkrun.org.uk","/prospect", "Hampshire Cross Country League event in the park"),
-
-                    new ParkrunCancellation(new DateTime(2019,01,19), "Bramley parkrun",
-                        "www.parkrun.org.uk","/bramley", "removal of asbestos from a park facility"),
-
+                    new ParkrunCancellation(new DateTime(2022, 12, 31), "Chasewater parkrun",
+                        "www.parkrun.org.uk", "/chasewater", "Request by landowner due to other event in park"),
+                    new ParkrunCancellation(new DateTime(2022, 12, 31), "Chipping Norton School parkrun",
+                        "www.parkrun.org.uk", "/chippingnortonschool", "Core team unavailable"),
                 });
             }
         }
@@ -66,7 +43,6 @@ namespace ParkrunMap.Scraping.Tests.Cancellations
 
                 cancellations.Should().BeEmpty();
             }
-
         }
     }
 }
